91e373c73095352d65d92a8e75200431bccc457d
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2024-04-05 19:17:22 -0300

    Update exerc_06.py

--- exerc_06.py
+++ exerc_06.py
@@ -1,10 +1,10 @@
 # Contar a quantidade de digitos de um número 
 def contar_digitos(numero):
-    qtd_digitos = len(str(numero))
+    qtd_digitos = len(numero)
     return qtd_digitos
 
 def main():
-    numero = int(input("Informe o número: "))
+    numero = input("Informe o número: ")
     qtd_digitos = contar_digitos(numero)
     print(f"Quantidade de digitos: {qtd_digitos}")
 
